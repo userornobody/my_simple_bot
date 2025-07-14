54d9052c24e565369ec5cf5a5712d57d8b3f6a55
userornobody <userornobody@gmail.com>
2025-07-15 03:50:25 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
-BOT_TOKEN = "SIZNING_BOT_TOKENINGIZ"
+BOT_TOKEN = "7566837278:AAEDBWzXtqk21JzTRbYewZAKtpi5apEcx1s"
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Siz yozdingiz: {update.message.text}")
